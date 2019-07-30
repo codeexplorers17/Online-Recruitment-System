1b261dd3eb5851026d9efb26bd4890201a427b16
ImeshaPiyumali <piyumaliimesha@gmail.com>
2019-07-30 21:01:58 +0530

    adding all files initially

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgxEditorModule } from 'ngx-editor';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    NgxEditorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
